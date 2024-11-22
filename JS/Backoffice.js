32c3a27f98628a6800ebf0d346f7dd2b2be6f707
annunziatadr99 <anudar99@hotmail.it>
2024-11-22 16:56:48 +0100

    settato pulsante reset

--- JS/Backoffice.js
+++ JS/Backoffice.js
@@ -4,18 +4,16 @@ const URL = id
   ? "https://striveschool-api.herokuapp.com/api/product/" + id
   : "https://striveschool-api.herokuapp.com/api/product/"; // ? e : sono come un if else questo metodo viene chiamto TERNARY OPERETOR
 const method = id ? "PUT" : "POST";
-const apiKay =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzM3MTRhODhhZDEyOTAwMTU4NzZiZGMiLCJpYXQiOjE3MzE2NjMwMTYsImV4cCI6MTczMjg3MjYxNn0.VVE_bglUNJS2naNrpNQ9P1yOgs4TrjYoFQylcmEPqDo";
 
 const handleSubmit = (e) => {
   e.preventDefault();
 
   const newProduct = {
-    name: e.target.elements.name.value,
-    description: e.target.elements.description.value,
-    brand: e.target.elements.brand.value,
-    imageUrl: e.target.elements.imageUrl.value,
-    price: e.target.elements.price.value,
+    name: e.target.name.value,
+    description: e.target.description.value,
+    brand: e.target.brand.value,
+    imageUrl: e.target.imageUrl.value,
+    price: e.target.price.value,
   };
   fetch(URL, {
     method: method, // richiamiamo il method della costante creata sopra
@@ -45,12 +43,13 @@ const handleSubmit = (e) => {
 const handleDelete = () => {
   const confirmed = confirm("sei sicuro di voler eliminare la risorsa?");
   if (confirmed) {
-    fetch(URL, { 
-        method: "DELETE",
-         headers: {
-            Authorization:
-              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzM3MTRhODhhZDEyOTAwMTU4NzZiZGMiLCJpYXQiOjE3MzE2NjMwMTYsImV4cCI6MTczMjg3MjYxNn0.VVE_bglUNJS2naNrpNQ9P1yOgs4TrjYoFQylcmEPqDo",
-          } })
+    fetch(URL, {
+      method: "DELETE",
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzM3MTRhODhhZDEyOTAwMTU4NzZiZGMiLCJpYXQiOjE3MzE2NjMwMTYsImV4cCI6MTczMjg3MjYxNn0.VVE_bglUNJS2naNrpNQ9P1yOgs4TrjYoFQylcmEPqDo",
+      },
+    })
       .then((resp) => {
         if (resp.ok) {
           return resp.json();
@@ -58,23 +57,29 @@ const handleDelete = () => {
       })
       .then((deletedObj) => {
         alert(
-          "la risorsa id " +
-            deletedObj._id +
-            " è stata eliminata CORRETTAMENTE"
+          "la risorsa id " + deletedObj._id + " è stata eliminata CORRETTAMENTE"
         );
         window.location.assign("/");
       })
       .catch((err) => console.log(err));
   }
 };
+const handleReset = () => {
+  const hasConfirmed = alert("I campi sono stati Resettati");
+  const form = document.getElementById("form");
+  if (hasConfirmed) {
+    form.reset();
+  }
+};
 
 window.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("form");
+  const form = document.getElementById("form");
   form.onsubmit = handleSubmit;
 
   const submitBtn = document.getElementById("submitBtn");
   const deleteBtn = document.getElementById("deleteBtn");
   const subtitle = document.getElementById("subtitle");
+  const resetBtn = document.getElementById("reset");
 
   if (id) {
     subtitle.innerText = "-Modifica Prodotto";
@@ -82,8 +87,15 @@ window.addEventListener("DOMContentLoaded", function () {
     submitBtn.classList.add("btn-primary");
     deleteBtn.classList.remove("d-none");
     deleteBtn.onclick = handleDelete;
+    resetBtn.classList.remove("d-none");
+    resetBtn.onclick = handleReset;
 
-    fetch(URL)
+    fetch(URL, {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NzM3MTRhODhhZDEyOTAwMTU4NzZiZGMiLCJpYXQiOjE3MzE2NjMwMTYsImV4cCI6MTczMjg3MjYxNn0.VVE_bglUNJS2naNrpNQ9P1yOgs4TrjYoFQylcmEPqDo",
+      },
+    })
       .then((resp) => {
         if (resp.ok) {
           return resp.json();
